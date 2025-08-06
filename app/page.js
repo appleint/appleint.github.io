8247c99f3ee0168d5a013bc707fbd37c209887b2
thriving27 <singlecycle27@163.com>
2025-08-06 19:14:24 +0800

    ui: add layout

--- app/page.js
+++ app/page.js
@@ -1,20 +1,21 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold mb-6">Welcome to My Next.js App</h1>
-      <Image
-        src="/images/hero-image.jpg"
-        alt="Hero Image"
-        width={600}
-        height={400}
-        className="rounded-lg shadow-lg"
-      />
-      <p className="mt-4 text-lg text-gray-700">
-        This is a simple example of a Next.js application using Tailwind CSS for
-        styling.
-      </p>
+    <div className="flex size-full min-h-screen flex-col items-center bg-gray-100">
+      <div className="h-10 w-full bg-blue-500 p-2">
+        <h1 className="mb-6 text-4xl font-bold text-black">
+          Welcome to My Next.js App
+        </h1>
+      </div>
+      <div className="w-full flex-1 bg-blue-100">
+        <p className="mt-4 text-lg text-gray-700">
+          This is a simple example of a Next.js application using Tailwind CSS
+          for styling.
+        </p>
+      </div>
+
+      <div className="h-10 w-full bg-blue-500">
+        <footer className="text-black">Welcome!</footer>
+      </div>
     </div>
   );
 }
